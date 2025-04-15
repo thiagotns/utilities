50da88306e11b9d16301a0a3775b8d63f30e1853
Thiago Nunes de Sousa <thiagonunes.tns@gmail.com>
2025-04-15 16:33:15 -0300

    add prestador and tomador

--- nf-extractor/script.py
+++ nf-extractor/script.py
@@ -14,15 +14,21 @@ SAIDA_CSV = sys.argv[2]
 regex_valor = re.compile(r"VALOR TOTAL DA NOTA = R\$ ?([\d\.,]+)")
 regex_mes_ano = re.compile(r"Mês de (\d{2}/\d{4})")
 regex_servico = re.compile(r"(?i)(SESSÕES DE .+?)\s*\n")
-regex_prestador = re.compile(r"Nome / Razão Social:\s*([^\n]+)")
+regex_prestador_nome = re.compile(r"Nome / Razão Social:\s*([^\n]+)")
+regex_prestador_doc = re.compile(r"CPF / CNPJ:\s*([\d\.\-/]+)")
 regex_quantidade = re.compile(r"Item\s+Quantidade\s+Valor Unitário.+?\n(\d+)")
+regex_tomador_nome = re.compile(r"TOMADOR DE SERVIÇOS\s+Nome / Razão Social:\s*([\w\s\-\.]+)")
+regex_tomador_doc = re.compile(r"TOMADOR DE SERVIÇOS.*?(\d{3}\.\d{3}\.\d{3}-\d{2})", re.DOTALL)
 
 def extrair_dados(texto):
     valor = regex_valor.search(texto)
     mes_ano = regex_mes_ano.search(texto)
     servico = regex_servico.search(texto)
-    prestador = regex_prestador.search(texto)
+    prestador_nome = regex_prestador_nome.search(texto)
+    prestador_doc = regex_prestador_doc.search(texto)
     quantidade = regex_quantidade.search(texto)
+    tomador_nome = regex_tomador_nome.search(texto)
+    tomador_doc = regex_tomador_doc.search(texto)
 
     mes, ano = (mes_ano.group(1).split("/") if mes_ano else (None, None))
 
@@ -31,8 +37,11 @@ def extrair_dados(texto):
         "mes_referencia": mes,
         "ano_referencia": ano,
         "tipo_servico": servico.group(1).strip().title() if servico else None,
-        "prestador": prestador.group(1).strip().title() if prestador else None,
-        "quantidade_sessoes": int(quantidade.group(1)) if quantidade else None
+        "prestador": prestador_nome.group(1).strip().title() if prestador_nome else None,
+        "prestador_cpf_cnpj": prestador_doc.group(1).strip() if prestador_doc else None,
+        "quantidade_sessoes": int(quantidade.group(1)) if quantidade else None,
+        "tomador": tomador_nome.group(1).strip().title() if tomador_nome else None,
+        "tomador_cpf": tomador_doc.group(1).strip() if tomador_doc else None,
     }
 
 dados = []
